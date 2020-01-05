a8582cf62d744a39a45619663cd1315e85cad2ad
KwangKyunBae <bae7359721@naver.com>
2020-01-05 06:06:43 +0000

    third commits

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Profile
 # Register your models here.
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id','nickname','user'] #id는 장고에서 자동생성
+    list_display_links = ['nickname','user']
+    search_fields = ['nickname']
